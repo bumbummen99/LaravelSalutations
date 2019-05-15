b8773925f9b498cfc293a4f58f38e7a2d1b5c773
Patrick Henninger <donnironon@hotmail.de>
2019-05-15 12:32:28 +0200

    Update composer.json to laravel 5.8
    Remove composer.lock
    
    Fixed tests for 5.8

--- tests/SalutationsTest.php
+++ tests/SalutationsTest.php
@@ -38,7 +38,7 @@ class SalutationsTest extends TestCase
      *
      * @return void
      */
-    protected function setUp()
+    protected function setUp() : void
     {
         parent::setUp();
     }
